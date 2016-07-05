cefccd5a5c736ed19317e806ae79d3186a5af901
Rico Sta. Cruz <rstacruz@users.noreply.github.com>
2016-07-06 00:48:48 +0800

    Initial implementation

---
+++ brunch-config.js
@@ -0,0 +1,47 @@
+// See http://brunch.io/#documentation for docs.
+exports.config = {
+  files: {
+    javascripts: {
+      joinTo: 'js/app.js'
+    },
+    stylesheets: {
+      joinTo: 'css/app.css'
+    },
+    templates: {
+      joinTo: 'js/app.js'
+    }
+  },
+
+  conventions: {
+    // This option sets where we should place non-css and non-js assets in.
+    assets: /^src\/assets\//
+  },
+
+  paths: {
+    watched: ["src"],
+    public: "public" // Where to compile files to
+  },
+
+  plugins: {
+    sass: {
+      options: {
+        includePaths: [
+          'node_modules'
+        ]
+      }
+    },
+    postcss: {
+      processors: [
+        require('autoprefixer')()
+      ]
+    },
+    jaded: {
+      staticPatterns:
+        /^src(\/|\\)html(\/|\\)(.+)\.jade$/
+    }
+  },
+
+  npm: {
+    enabled: true
+  }
+}
